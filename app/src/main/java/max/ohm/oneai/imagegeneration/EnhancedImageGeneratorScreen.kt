f74db7a412b8f3f6d5f362a687e4cc16a856227e
maxohm1 <maxohm24@gmail.com>
2025-08-03 01:29:37 +0530

    remove some image generation model

--- app/src/main/java/max/ohm/oneai/imagegeneration/EnhancedImageGeneratorScreen.kt
+++ app/src/main/java/max/ohm/oneai/imagegeneration/EnhancedImageGeneratorScreen.kt
@@ -116,13 +116,13 @@ fun EnhancedImageGeneratorScreen(
     // Model selection
     var modelMenuExpanded by remember { mutableStateOf(false) }
     val modelChoices = listOf(
-        ModelChoice("Flux Schnell", "flux.1-schnell"),
+        //ModelChoice("Flux Schnell", "flux.1-schnell"),
         // ModelChoice("Image-1", "provider-5/gpt-image-1"),
         ModelChoice("ImageGen-4", "provider-4/imagen-4"),
         ModelChoice("ImageGen-3", "provider-4/imagen-3"),
         // ModelChoice("FLUX Kontext Max", "provider-2/FLUX.1-kontext-max"),
         ModelChoice("FLUX Kontext Pro", "provider-1/FLUX.1-kontext-pro"),
-        ModelChoice("Flux Pro Raw", "provider-3/FLUX.1.1-pro-ultra-raw"),
+        //ModelChoice("Flux Pro Raw", "provider-3/FLUX.1.1-pro-ultra-raw"),
         ModelChoice("Flux Pro", "provider-1/FLUX.1.1-pro"),
         ModelChoice("Flux Ultra Pro", "provider-3/FLUX.1.1-pro-ultra"),
         ModelChoice("DALL-E 3", "provider-3/dall-e-3"),
