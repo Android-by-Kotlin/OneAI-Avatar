63d8c9cfa7e62c18bc3ae3a5d7a30023b9a0d329
maxohm1 <maxohm24@gmail.com>
2025-08-04 20:23:38 +0530

    previosely remove the foreground service in livekit and now remove the video media permission from the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "max.ohm.oneai"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
