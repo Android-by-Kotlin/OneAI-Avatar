beecd906899c0d4035611cd61977b62f29d8fb3c
maxohm1 <maxohm24@gmail.com>
2025-08-13 03:44:47 +0530

    change in UI

--- app/src/main/java/max/ohm/oneai/ImagetoImage/modelslabapikey/modelslabapikey.kt
+++ app/src/main/java/max/ohm/oneai/ImagetoImage/modelslabapikey/modelslabapikey.kt
@@ -1,3 +1,5 @@
 package max.ohm.oneai.imagetoimage.modelslabapikey
 
-  const val MODELSLAB_API_KEY = "0ihyp1prSphXcexPXOwm1DMvpsvLOq995x7gXRAkNmccQ7HRFGcb9stRuDvm"
+//const val MODELSLAB_API_KEY = "0ihyp1prSphXcexPXOwm1DMvpsvLOq995x7gXRAkNmccQ7HRFGcb9stRuDvm"
+
+const val MODELSLAB_API_KEY = "0TZLnFEqdrVZ3QlDT7KwcAaJv99OFGrmVBphSNTz5qUcWXWcUJQ58kQbxhxx"
